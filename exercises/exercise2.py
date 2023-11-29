92b735f5971fbedb5014c69d4819d085ed6ca838
Muhammad Faraz Mazhar <ho61tofo@fauad.fau.de>
2023-11-29 11:08:19 +0100

    exercise2 update

--- exercises/exercise2.py
+++ exercises/exercise2.py
@@ -1,65 +1,51 @@
 import pandas as pd
-import requests
-from io import StringIO
 import sqlite3
 
 # Define the CSV URL
 csv_url = "https://download-data.deutschebahn.com/static/datasets/haltestellen/D_Bahnhof_2020_alle.CSV"
 
-
-# Function to download and process the CSV
+# Function to process the CSV and insert data into SQLite
 def process_csv(csv_url, database_name="trainstops.sqlite", table_name="trainstops"):
-    # Download CSV file
-    response = requests.get(csv_url)
+    # Read CSV data into a DataFrame directly from the URL
+    df = pd.read_csv(csv_url, sep=';', decimal=',')
 
-    # Check if the request was successful (status code 200)
-    if response.status_code == 200:
-        # Read CSV data into a DataFrame
-        csv_data = StringIO(response.text)
-        df = pd.read_csv(csv_data, sep=';', decimal=',')  # Adjust separator based on the actual CSV format
+    # Drop the "Status" column
+    df = df.drop(columns=['Status'], errors='ignore')
 
-        # Drop the "Status" column
-        df = df.drop(columns=['Status'], errors='ignore')
+    # Drop rows with invalid values
+    df = df.dropna()
 
-        # Drop rows with invalid values
-        df = df.dropna()  # Drop rows with any NaN values
+    # Filter rows based on specified conditions
+    df = df[df['Verkehr'].isin(['FV', 'RV', 'nur DPN'])]
+    df = df[(df['Laenge'] >= -90) & (df['Laenge'] <= 90)]
+    df = df[(df['Breite'] >= -90) & (df['Breite'] <= 90)]
+    df = df[df['IFOPT'].str.match(r'^[A-Za-z]{2}:\d+:\d+(\:\d+)?$')]
 
-        # Filter rows based on specified conditions
-        df = df[df['Verkehr'].isin(['FV', 'RV', 'nur DPN'])]
-        df = df[(df['Laenge'] >= -90) & (df['Laenge'] <= 90)]
-        df = df[(df['Breite'] >= -90) & (df['Breite'] <= 90)]
-        df = df[df['IFOPT'].str.match(r'^[A-Za-z]{2}:\d+:\d+(\:\d+)?$')]
+    # SQLite connection and cursor
+    conn = sqlite3.connect(database_name)
+    cursor = conn.cursor()
 
-        # SQLite connection and cursor
-        conn = sqlite3.connect(database_name)
-        cursor = conn.cursor()
+    # Create the "trainstops" table
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS trainstops (
+            Id INTEGER PRIMARY KEY AUTOINCREMENT,
+            BahnhofId TEXT,
+            Name TEXT,
+            Verkehr TEXT,
+            Laenge FLOAT,
+            Breite FLOAT,
+            IFOPT TEXT
+        )
+    ''')
 
-        # Create the "trainstops" table
-        cursor.execute('''
-            CREATE TABLE IF NOT EXISTS trainstops (
-                EVA_NR BIGINT,
-                DS100 TEXT,
-                IFOPT TEXT,
-                NAME TEXT,
-                Verkehr TEXT,
-                Laenge FLOAT,
-                Breite FLOAT,
-                Betreiber_Name TEXT,
-                Betreiber_Nr BIGINT
-            )
-        ''')
+    # Insert data into the "trainstops" table
+    df.to_sql(name=table_name, con=conn, index=False, if_exists='replace')
 
-        # Insert data into the "trainstops" table
-        df.to_sql(name=table_name, con=conn, index=False, if_exists='replace')
+    # Commit changes and close connection
+    conn.commit()
+    conn.close()
 
-        # Commit changes and close connection
-        conn.commit()
-        conn.close()
-
-        print("Data processing and insertion into SQLite completed.")
-    else:
-        print(f"Failed to download CSV. Status code: {response.status_code}")
-
+    print("Data processing and insertion into SQLite completed.")
 
 # Execute the pipeline
 process_csv(csv_url)
