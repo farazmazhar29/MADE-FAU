39839416b22a9ce1259e0aa9276a98fea2db689e
Muhammad Faraz Mazhar <ho61tofo@fauad.fau.de>
2024-01-23 19:17:51 +0100

    exercise4.py

---
+++ exercises/exercise4.py
@@ -0,0 +1,36 @@
+import urllib.request
+import zipfile
+import pandas as pd
+import sqlalchemy
+
+URL = "https://www.mowesta.com/data/measure/mowesta-dataset-20221107.zip"
+
+urllib.request.urlretrieve(URL, "data.zip")
+with zipfile.ZipFile('data.zip', 'r') as zip_ref:
+    zip_ref.extractall("../exercises/")
+fields = ["Geraet", "Hersteller", "Model", "Monat", "Temperatur in °C (DWD)", "Batterietemperatur in °C",
+          "Geraet aktiv"]
+
+df = pd.read_csv('../exercises/data.csv', sep=";", index_col=False, usecols=fields, encoding='utf-8', decimal=",")
+df.rename(columns={"Temperatur in °C (DWD)": "Temperatur", "Batterietemperatur in °C": "Batterietemperatur"},
+          inplace=True)
+df['Temperatur'] = (df['Temperatur'] * 9 / 5) + 32
+df['Batterietemperatur'] = df['Batterietemperatur']
+
+# Validate data
+df = df.loc[df['Geraet'] > 0]
+df = df.loc[(df['Monat'] >= 1) & (df['Monat'] <= 12)]
+df = df.loc[(df['Temperatur'] >= -459.67) & (df['Temperatur'] <= 212)]
+df = df.loc[(df['Batterietemperatur'] >= -459.67) & (df['Batterietemperatur'] <= 212)]
+valid_gear_aktiv_values = ["Ja", "Nein"]
+df = df.loc[(df['Geraet aktiv'].isin(valid_gear_aktiv_values))]
+
+df.to_sql('temperatures', 'sqlite:///temperatures.sqlite', if_exists='replace', index=False, dtype={
+    "Geraet": sqlalchemy.BIGINT,
+    "Hersteller": sqlalchemy.TEXT,
+    "Model": sqlalchemy.TEXT,
+    "Monat": sqlalchemy.BIGINT,
+    "Temperatur": sqlalchemy.FLOAT,
+    "Batterietemperatur": sqlalchemy.FLOAT,
+    "Geraet aktiv": sqlalchemy.TEXT,
+})
